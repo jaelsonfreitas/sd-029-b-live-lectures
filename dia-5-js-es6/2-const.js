56d4ec1dea08008d688c1ad2c84966f4c281e7ef
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-01 17:22:51 -0300

    Const.

---
+++ dia-5-js-es6/2-const.js
@@ -0,0 +1,19 @@
+//A PEGA DA VARIÁVEL CONSTANTE
+const trybe = 'Turma 29';
+
+// trybe = 'Turma 29 - B';
+
+// console.log(trybe);
+
+const smartphones = {
+  'Samsung Galaxy S20': 2300,
+  'Moto G52': 1200,
+  'Samsung Galaxy A53': 2200,
+  'Nokia C01 Plus': 550, 
+}
+
+smartphones['iPhone 14 Pro'] = 100000;
+
+smartphones = 'oi';
+
+console.log(smartphones);
