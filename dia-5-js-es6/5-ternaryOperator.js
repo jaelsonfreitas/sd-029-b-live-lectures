9031991807eb8e38ef0d04ab919df64dfbe24d38
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-01 17:25:41 -0300

    Operador ternario.

---
+++ dia-5-js-es6/5-ternaryOperator.js
@@ -0,0 +1,12 @@
+//OPERADOR TERNÁRIO
+//BRASIL HEXA ? ****** : EU SOU MALUCO
+
+const evenOrOdd = (number) => number % 2 === 0 ? 'PAR' : 'ÍMPAR';
+
+let number = 741;
+
+console.log(`O número ${number} é: ${evenOrOdd(number)}.`);
+
+// = aribuição
+// == igualdade de valores
+// === igualdade de valores e tipos
