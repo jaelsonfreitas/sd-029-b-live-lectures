21f83c5c2000cb7f7fedbc0eada6574d2c9ba32f
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-02 22:32:48 -0300

    Criacao arquivo js e exemplo de array.

---
+++ secao-4-introducao-a-javascript/dia-2-js-array-loop-for/array-and-loops.js
@@ -0,0 +1,10 @@
+const lotteryNumbers = [14, 26, 56, 39, 27, 5];
+
+console.log(lotteryNumbers[0]);
+console.log(lotteryNumbers[1]);
+console.log(lotteryNumbers[2]);
+console.log(lotteryNumbers[3]);
+console.log(lotteryNumbers[4]);
+console.log(lotteryNumbers[5]);
+
+console.log(lotteryNumbers.length);
